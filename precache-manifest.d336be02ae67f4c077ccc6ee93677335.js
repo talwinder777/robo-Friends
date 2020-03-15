cb3fbcac5e1351dd5fcc302838d9055b47d2a42d
talwinder777 <stalwinder777@gmail.com>
2020-03-15 17:11:00 -0500

    Updates

---
+++ precache-manifest.d336be02ae67f4c077ccc6ee93677335.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340d4c14d0635e59562da0b36e91c14e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dc51e56adc7f996e62a",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8bfd059a99d59f0dace5",
+    "url": "/static/css/main.9253224d.chunk.css"
+  },
+  {
+    "revision": "6dc51e56adc7f996e62a",
+    "url": "/static/js/2.23af81ae.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.23af81ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfd059a99d59f0dace5",
+    "url": "/static/js/main.467a8801.chunk.js"
+  },
+  {
+    "revision": "29247c02f79741d4d17e",
+    "url": "/static/js/runtime-main.44525653.js"
+  }
+]);
